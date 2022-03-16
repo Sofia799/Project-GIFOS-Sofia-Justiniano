d207e6428563aaf6f6278392a87d38551c673eaf
Sofia Justiniano <sofia.agustina.justiniano@gmail.com>
2022-03-16 15:17:34 -0300

    fondo modo oscuro

--- Script/dark-mode.js
+++ Script/dark-mode.js
@@ -1,6 +1,5 @@
 function cambiarModo() {
     let objDark = document.querySelectorAll(".dark");
-    let blackline = document.querySelectorAll(".blackLine")
 
     const theme = document.getElementById("theme");
 
@@ -18,8 +17,6 @@ function cambiarModo() {
         }
     }
 
-
-
     let objMoreDark = document.querySelectorAll(".moreDark");
 
     for (const obj of objMoreDark) {
@@ -30,102 +27,4 @@ function cambiarModo() {
         }
     }
 
-
-
-    // cambiarColorTexto()
-
 }
-
-function cambiarColorTexto() {
-
-    const whiteTexts = document.getElementsByClassName("whiteText");
-
-
-    for (let i = 0; i < whiteTexts.length; i++) {
-
-        const textColor = whiteTexts[i].style.color;
-        console.log(textColor)
-
-        // switch (textColor) {
-        //     case "black":
-        //     case "#572EE5":
-        //         textColor = "white";
-        //         break;
-
-        //     default:
-        //         textColor = "572EE5"
-        //         break;
-        // }
-    }
-
-
-}
-// for (const texto of textos) {
-//     if (texto.) {
-
-//     }
-// }
-
-
-
-
-// let header = document.getElementById("header");
-// let logoHeader = document.getElementsByClassName("img-logo-gifos");
-
-// let nav = document.getElementById("nav");
-
-// let sectionBusqGifos = document.getElementById("container-busqGifos");
-// let trending = document.getElementById("trending");
-// let trendingGifos = document.getElementById("container-gifs");
-// let footer = document.getElementById("container-footer");
-// let lineHeader = document.getElementById("linea-azul");
-// let lineFooter = document.getElementById("linea-azul-footer");
-
-// if (header.className == "light") {
-//     header.classList.toggle("dark");
-// } else {
-//     header.classList.toggle("light")
-//         //changeLogo(logoHeader);
-// }
-
-// if (nav.className == "blue") {
-//     nav.classList.toggle("moreDark");
-// } else {
-//     nav.classList.toggle("blue");
-// }
-
-// if (sectionBusqGifos.className == "light") {
-//     sectionBusqGifos.classList.toggle("dark");
-// } else {
-//     sectionBusqGifos.classList.toggle("light");
-// }
-
-// if (trending.className == "light") {
-//     trending.classList.toggle("dark");
-// } else {
-//     trending.classList.toggle("light");
-// }
-
-// if (trendingGifos.className == "light2") {
-//     trendingGifos.classList.toggle("moreDark");
-// } else {
-//     trendingGifos.classList.toggle("light2");
-// }
-
-// if (footer.className == "light") {
-//     footer.classList.toggle("dark");
-// } else {
-//     footer.classList.toggle("light");
-// }
-
-// if (lineHeader.className == "blue-line") {
-//     lineHeader.classList.toggle("black-line")
-// } else {
-//     lineHeader.classList.toggle("blue-line");
-// }
-
-// if (lineFooter.className == "blue-line") {
-//     lineFooter.classList.toggle("black-line")
-// } else {
-//     lineFooter.classList.toggle("blue-line")
-// }
